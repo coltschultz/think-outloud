7c1ec51a526f9986493f7088b776d75f76d18832
Colt <98503459+coltschultz@users.noreply.github.com>
2022-06-02 17:32:22 -0500

    api routes

--- controllers/reaction-controller.js
+++ controllers/reaction-controller.js
@@ -1,13 +1,13 @@
 const { Reaction, Thought } = require('../models');
 
-const reactionController = {
-  // add reaction to thought
-  addReaction({ req.body, body }, res) {
+const commentController = {
+  // add comment to thought
+  addReaction({ params, body }, res) {
     console.log(body);
     Reaction.create(body)
       .then(({ _id }) => {
         return Thought.findOneAndUpdate(
-          { _id: req.body.pizzaId },
+          { _id: params.thoughtId },
           { $push: { reactions: _id } },
           { new: true }
         );
@@ -22,16 +22,16 @@ const reactionController = {
       .catch(err => res.json(err));
   },
 
-  // remove reaction
-  removeReaction({ req.body }, res) {
-    Reaction.findOneAndDelete({ _id: req.body.reactionId })
+  // remove comment
+  removeReaction({ params }, res) {
+    Reaction.findOneAndDelete({ _id: params.reactionId })
       .then(deletedReaction => {
         if (!deletedReaction) {
-          return res.status(404).json({ message: 'No reaction with this id!' });
+          return res.status(404).json({ message: 'No comment with this id!' });
         }
         return Thought.findOneAndUpdate(
-          { _id: req.body.pizzaId },
-          { $pull: { reactions: req.body.reactionId } },
+          { _id: params.thoughtId },
+          { $pull: { reactions: params.reactionId } },
           { new: true }
         );
       })
