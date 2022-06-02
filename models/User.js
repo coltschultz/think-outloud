7d1144403bc003b1cb334734a89a11828aadf1a3
Colt <98503459+coltschultz@users.noreply.github.com>
2022-06-02 00:36:42 -0500

    models

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unqiue: true,
+    // add validation
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+
+const User = model('User', UserSchema);
+
+module.exports = User;
